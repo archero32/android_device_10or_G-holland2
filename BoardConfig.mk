0a25e08989bb13ccef47db69098f6743862c31b9
Lalit <123730669+archero32@users.noreply.github.com>
2023-05-26 08:37:11 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,4 +185,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
-include vendor/10or/holland2/BoardConfigVendor.mk
+include vendor/10or/G/BoardConfigVendor.mk
